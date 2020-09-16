42f3306eaeefd9d27c3f001b6e4b4863407f10ae
robinmduong <robinmduong@gmail.com>
2020-09-15 19:09:49 -0700

    Make changes to Controllers

--- BulkyBookDataAccess/Data/ApplicationDbContext.cs
+++ BulkyBookDataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,6 @@ namespace BulkyBook.DataAccess.Data
         {
         }
 
-        public DbSet<Category> Categories { get; set;  }
+        public DbSet<Category> Categories { get; set; }
     }
 }
