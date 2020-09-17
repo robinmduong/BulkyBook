7e94cefcdfd851b9ba94075a8a5e7afb9346eacd
robinmduong <robinmduong@gmail.com>
2020-09-16 17:46:30 -0700

    Adding Upsert functionality

--- BulkyBook/Areas/Admin/Controllers/CategoryController.cs
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBook.Areas.Admin.Controllers
@@ -21,6 +22,28 @@ namespace BulkyBook.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        //the id is optional (?) because you only need it if you're updating, not when inserting
+        public IActionResult Upsert(int? id) //R click on Upsert and add MVC view
+        {
+            Category category = new Category();
+
+            if (id == null)
+            {
+                //this is for create, that's why there's no id
+                return View(category);
+            }
+
+            //this is for edit
+
+            category = _unitOfWork.Category.Get(id.GetValueOrDefault());
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
         }
 
         #region API CALLS
