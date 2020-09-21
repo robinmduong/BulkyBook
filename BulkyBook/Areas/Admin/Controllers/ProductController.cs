277fdadd4ab1b3d1a4731d48559729e58b626ef3
robinmduong <robinmduong@gmail.com>
2020-09-20 21:51:55 -0700

    Code for product, but has bugs

--- BulkyBook/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBook/Areas/Admin/Controllers/ProductController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyBook.Areas.Admin.Controllers //after this, we need to work on js
 {
@@ -22,8 +25,8 @@ namespace BulkyBook.Areas.Admin.Controllers //after this, we need to work on js
             _unitOfWork = unitOfWork;
             _hostEnvironment = hostEnvironment;
         }
-        
-        public IActionResult Index()
+
+        public IActionResult Index() //Right-click on Index and then add Index View, or Index.cshtml
         {
             return View();
         }
@@ -31,41 +34,96 @@ namespace BulkyBook.Areas.Admin.Controllers //after this, we need to work on js
         //the id is optional (?) because you only need it if you're updating, not when inserting
         public IActionResult Upsert(int? id) //R click on Upsert and add MVC view
         {
-            Product product = new Product();
+            ProductVM productVM = new ProductVM()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                }),
+                CoverTypeList = _unitOfWork.CoverType.GetAll().Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
 
             if (id == null)
             {
                 //this is for create, that's why there's no id
-                return View(product);
+                return View(productVM);
             }
 
             //this is for edit
-            product = _unitOfWork.Product.Get(id.GetValueOrDefault());
-            if (product == null)
+            productVM.Product = _unitOfWork.Product.Get(id.GetValueOrDefault());
+            if (productVM == null)
             {
                 return NotFound();
             }
-            return View(product);
+            return View(productVM);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken] //if cookie val != form val, error raised
-        public IActionResult Upsert(Product product)
+        public IActionResult Upsert(ProductVM productVM)
         {
             if (ModelState.IsValid)
             {
-                if (product.Id == 0)
+                string webRootPath = _hostEnvironment.WebRootPath;
+                var files = HttpContext.Request.Form.Files; //retrieve all files that were uploaded
+                if(files.Count > 0) //files was uploaded
                 {
-                    _unitOfWork.Product.Add(product);
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\products");
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    if(productVM.Product.ImageUrl != null)
+                    {
+                        // this is an edit, so we need to remove old image
+                        var imagePath = Path.Combine(webRootPath, productVM.Product.ImageUrl.TrimStart('\\')); //remove the back slashes from the start
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+                        using (var filesStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                        {
+                            files[0].CopyTo(filesStreams);
+                        }
+                        productVM.Product.ImageUrl = @"\images\product" + fileName + extension;
+                    }
+                    else
+                    {
+                        //update when they do not change image
+                        if(productVM.Product.Id != 0)
+                        {
+                            Product objFromDb = _unitOfWork.Product.Get(productVM.Product.Id);
+                            productVM.Product.ImageUrl = objFromDb.ImageUrl;
+                        }
+                    }
+
+                    if (productVM.Product.Id == 0)
+                    {
+                        _unitOfWork.Product.Add(productVM.Product);
+                    }
+                    else
+                    {
+                        _unitOfWork.Product.Update(productVM.Product);
+                    }
+                    return View(productVM);
+                }
+
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
                 }
                 else
                 {
-                    _unitOfWork.Product.Update(product);
+                    _unitOfWork.Product.Update(productVM.Product);
                 }
                 _unitOfWork.Save();
                 return RedirectToAction(nameof(Index)); //we don't type in "Index" directly because we want to avoid using magic string
             }
-            return View(product);
+            return View(productVM);
         }
 
         #region API CALLS
@@ -73,19 +131,19 @@ namespace BulkyBook.Areas.Admin.Controllers //after this, we need to work on js
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.Category.GetAll();
+            var allObj = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return Json(new { data = allObj });
         }
 
         [HttpDelete]
         public IActionResult Delete(int id)
         {
-            var objFromDb = _unitOfWork.Category.Get(id);
-            if(objFromDb == null)
+            var objFromDb = _unitOfWork.Product.Get(id);
+            if (objFromDb == null)
             {
                 return Json(new { success = false, message = "Error while deleting" });
             }
-            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Product.Remove(objFromDb);
             _unitOfWork.Save();
             return Json(new { success = true, message = "Delete successful" });
         }
---
+++ BulkyBook/wwwroot/js/product.js
@@ -0,0 +1,65 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+//Make sure casing is correct
+//Make sure DataTable was imported in _Layout.cshtml
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "15%"},
+            { "data": "isbn", "width": "15%"},
+            { "data": "price", "width": "15%"},
+            { "data": "author", "width": "15%"},
+            //in ProductController.cs, we have this line of code, so that's why we can access the below information
+            //            var allObj = _unitOfWork.Category.GetAll(includeProperties:"Category,CoverType");
+            { "data": "category.name", "width": "15%"},
+            {
+                "data": "id",
+                "render": function (data) { //the data is the id
+                    return `
+                            <div class="text-center">
+                                <a href="/Admin/Product/Upsert/${data}" class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fa fa-edit"></i>
+                                </a>
+                                <a onclick=Delete("/Admin/Product/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fa fa-trash-alt"></i>
+                                </a>
+                                </div>
+                            `;
+                }, "width": "40%"
+            }
+        ]
+    })
+}
+
+function Delete(url) {
+    swal({ //using Sweet Alert
+        title: "Are you sure you want to Delete?",
+        text: "You will not be able to restore the data.",
+        icon: "warning",
+        buttons: true, //plural "buttons"
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
